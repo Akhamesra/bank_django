6fd7900d8d8f5e4e261aa14e4c3e04f9ce71019b
Aryamaan Pandey <aryamaanpandey@L-TMVR6D67G9.local>
2023-02-08 19:35:59 +0530

    url testing

--- final/accounts/tests.py
+++ final/accounts/tests.py
@@ -5,11 +5,17 @@ from . import views
 class TestUrls(SimpleTestCase):
 
     def test_url_is_resolved_login(self):
-        url = reverse('accounts:login')
-        self.assertEquals(resolve(url).func,views.login)
+        url = reverse('accounts:signin')
+        self.assertEquals(resolve(url).func,views.sign_in)
 
     def test_url_is_resolved_register(self):
-        url2 = reverse('accounts:register')
+        url2 = reverse('accounts:signup')
         self.assertEquals(resolve(url2).func,views.register)
+
+    def test_url_is_resolved_logout(self):
+        url3 = reverse('accounts:logout')
+        self.assertEquals(resolve(url3).func,views.logout_view)
+
+    
     
         
